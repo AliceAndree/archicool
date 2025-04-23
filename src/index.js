b65c03a8a96ac1ac207bc2d72efc532d74aa5ed1
AliceAndree <thiel.alice@gmail.com>
2025-04-23 14:58:22 +0200

    adding Favicon + final design image

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import "./styles.css";
+import faviconSVG from "../src/assets/small-logo.svg";
 import heroSection from "./sections/heroSection";
 import projectsSection from "./sections/projectsSection";
 import aboutSection from "./sections/aboutSection";
 import contactSection from "./sections/contactSection";
 import footerSection from "./sections/footerSection";
 
+const addFavicon = () => {
+  const favicon = document.querySelector("link[rel~='icon']");
+  favicon.href = faviconSVG;
+};
+
+addFavicon();
 heroSection();
 projectsSection();
 aboutSection();
